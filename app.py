e28e6a195a1ed7655914ee9fbc3edfbd687baa73
insecures <duchanhstyle@gmail.com>
2021-10-31 19:45:28 +0700

    process image with kmean

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # flask_ngrok_example.py
 from flask import Flask, jsonify, request, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+import kmean as KMeans
 # from flask_ngrok import run_with_ngrok
-
+  
 app = Flask(__name__,  static_url_path='')
+app.config['UPLOAD_FOLDER'] = "statics/image/"
+
 # run_with_ngrok(app)  
 @app.route('/statics/<path:path>')
 def send_js(path):
@@ -12,13 +16,21 @@ def send_js(path):
 def index():
     return render_template("index.html")
 
-
+# Route xử lý cụm
 @app.route("/cluster", methods=['POST'])
 def cluster():
-  uploaded_file = request.files['image'] 
-  print(uploaded_file)
+  image = request.files['image'] 
+  image.save(app.config['UPLOAD_FOLDER'] + secure_filename(image.filename))
+  colors = KMeans.get_colors(app.config['UPLOAD_FOLDER'] + secure_filename(image.filename), n_colors=2)
+  return jsonify(colors)
 
-  return jsonify([1])
-
+# Route xử lý get-colors
+@app.route("/get-colors", methods=['POST'])
+def getColors():
+  image = request.files['image'] 
+  image.save(app.config['UPLOAD_FOLDER'] + secure_filename(image.filename))
+  colors = KMeans.get_colors(app.config['UPLOAD_FOLDER'] + secure_filename(image.filename), n_colors=2)
+  return jsonify(colors)
+  
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
--- templates/app.js
+++ templates/app.js
@@ -1,14 +1,14 @@
 // ----- On render -----
-$(function() {
+$(function () {
 
   $('#profile').addClass('dragging').removeClass('dragging');
 });
 
-$('#profile').on('dragover', function() {
+$('#profile').on('dragover', function () {
   $('#profile').addClass('dragging')
-}).on('dragleave', function() {
+}).on('dragleave', function () {
   $('#profile').removeClass('dragging')
-}).on('drop', function(e) {
+}).on('drop', function (e) {
   $('#profile').removeClass('dragging hasImage');
 
   if (e.originalEvent) {
@@ -19,7 +19,7 @@ $('#profile').on('dragover', function() {
     //attach event handlers here...
 
     reader.readAsDataURL(file);
-    reader.onload = function(e) {
+    reader.onload = function (e) {
       alert(this.width + 'x' + this.height);
 
       // console.log(reader.result);
@@ -29,22 +29,22 @@ $('#profile').on('dragover', function() {
 
   }
 })
-$('#profile').on('click', function(e) {
+$('#profile').on('click', function (e) {
   console.log('clicked')
   $('#mediaFile').click();
 });
-window.addEventListener("dragover", function(e) {
+window.addEventListener("dragover", function (e) {
   e = e || event;
   e.preventDefault();
 }, false);
-window.addEventListener("drop", function(e) {
+window.addEventListener("drop", function (e) {
   e = e || event;
   e.preventDefault();
 }, false);
-$('#mediaFile').change(function(e) {
-  
+$('#mediaFile').change(function (e) {
+
   var input = e.target;
-  alertError(input.files[0],"Vui lòng upload ảnh !")
+  alertError(input.files[0], "Vui lòng upload ảnh !")
 
   if (input.files && input.files[0]) {
     var file = input.files[0];
@@ -52,7 +52,7 @@ $('#mediaFile').change(function(e) {
     var reader = new FileReader();
 
     reader.readAsDataURL(file);
-    reader.onload = function(e) {
+    reader.onload = function (e) {
       console.log(reader.result);
       $('#profile').css('background-image', 'url(' + reader.result + ')').addClass('hasImage');
     }
@@ -62,7 +62,7 @@ $('#mediaFile').change(function(e) {
 
 // Xuất lỗi
 function alertError(value, msg) {
-  if(!value || parseInt(value) <= 0) {
+  if (!value || parseInt(value) <= 0) {
     document.getElementById("alert").innerText = msg
     document.getElementById("alert").style.display = "block"
     return false;
@@ -72,24 +72,68 @@ function alertError(value, msg) {
 }
 
 // Validate number of cluster
-$("#cluster-num").keyup(function(e) {
- alertError(e.target.value,"Vui lòng số cluster sai (Yêu cầu > 0) !")
+$("#cluster-num").keyup(function (e) {
+  let valid = alertError(e.target.value, "Vui lòng số cluster sai (Yêu cầu > 0) !")
+  if (valid) {
+    $("#get-colors").prop('disabled', false);
+    return
+  }
+  $("#get-colors").prop('disabled', true);
 })
 
 
 // Optimized cluster
 
-$("#cal-cluster").click(async function(e) {
-  let input = document.getElementById("mediaFile");
-  let valid = alertError(input.files[0],"Vui lòng upload ảnh !")
-  if(!valid) return
-  let data = new FormData()
-  data.append("image", input.files[0])
+$("#cal-cluster").click(async function (e) {
+  // let input = document.getElementById("mediaFile");
+  // let valid = alertError(input.files[0], "Vui lòng upload ảnh !")
+  // if (!valid) return
+  // let data = new FormData()
+  // data.append("image", input.files[0])
 
-  let res = await fetch("/cluster", {
-    method : "POST",
+  // let res = await fetch("/cluster", {
+  //   method: "POST",
+  //   body: data
+  // })
+  // let dataRes = await res.json()
+  // console.log(dataRes);
+})
+
+// Lấy dữ liệu về màu và in ra màn hình
+$("#get-colors").click(async function (e) {
+  
+  $("#result").empty();
+
+  let inputImg = document.getElementById("mediaFile");
+  let inputCluster = document.getElementById("cluster-num");
+
+  // let valid = alertError(input.files[0], "Vui lòng upload ảnh !")
+  // if (!valid) return
+  let data = new FormData()
+  data.append("image", inputImg.files[0])
+  console.log("hello");
+  data.append("cluster", inputCluster.value)
+  let res = await fetch("/get-colors", {
+    method: "POST",
     body: data
   })
   let dataRes = await res.json()
-  console.log(dataRes);
- })
+  
+  for(let color of dataRes) {
+    console.log(color);
+    let box = document.createElement("div")
+    box.classList.add("box")
+
+    let colorEl = document.createElement("div")
+    colorEl.classList.add("color")
+    colorEl.style.backgroundColor = color;
+    box.appendChild(colorEl);
+
+    let textColor = document.createElement("p")
+    textColor.innerText = color;
+    box.appendChild(textColor);
+
+    $("#result").append(box);
+  }
+
+})
